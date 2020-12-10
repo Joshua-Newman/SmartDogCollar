78e9c2ed346f63a736dcf1cb2415e01721958a75
Joshua Newman <joshua.zips@gmail.com>
2020-12-10 12:09:34 -0500

    Time Fixed

--- demo_client.py
+++ demo_client.py
@@ -20,6 +20,28 @@ ssids = ['pi-mobile']
 # sudo argument automatixally gets set for 'false', if the 'true' is not set manually.
 # python file will have to be run with sudo privileges.
 
+import rssi
+from time import sleep, time
+
+
+# Python TCP Client A
+import socket
+
+host = socket.gethostname()
+port = 5005
+BUFFER_SIZE = 2000
+
+tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+interface = 'wlan0'
+rssi_scanner = rssi.RSSI_Scan(interface)
+
+ssids = ['pi-mobile']
+
+# sudo argument automatixally gets set for 'false', if the 'true' is not set manually.
+# python file will have to be run with sudo privileges.
+
 # Variables from power regression of RSSI data
 # distance=A*(r/t)^B+C
 # where r is the RSSI value and t is the RSSI at 1m
@@ -28,9 +50,9 @@ B = 3.63550212364731
 C = 0.1879191861
 t = -37
 
+now = time()
 rollingaverage = []
 while True:
-    now = time()
     MESSAGE = ""
     # get RSSI data
     ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
@@ -49,7 +71,7 @@ while True:
         MESSAGE += "Distance: {} m\n".format(distance)
     else:
         MESSAGE += "OUT OF RANGE\n"
-    if(now-time() > 5):  # collect data for 5 seconds, then send
+    if(time()-now > 5):  # collect data for 5 seconds, then send
         tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcpClientA.connect(("192.168.16.1", port))
         tcpClientA.sendall(MESSAGE.encode('utf-8'))
@@ -57,3 +79,4 @@ while True:
         print("sent data:")
         print(MESSAGE)
         tcpClientA.close()
+        now = time()
